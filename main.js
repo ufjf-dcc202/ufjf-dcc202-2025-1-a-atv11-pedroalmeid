5fbe6d8f8b58e7819de4e86ff2f549ee327099e1
Pedro José C.A <pedrojos.campos@protonmail.com>
2025-07-19 10:27:27 -0300

    cria index.html e main.js básicos

---
+++ main.js
@@ -0,0 +1,5 @@
+const olItens = document.querySelector("ol#itens");
+const pEntrada = document.querySelector("#entrada");
+const btnAdicionar = document.querySelector("#adicionar");
+const bttLimpar = document.querySelector("#limpar");
+
